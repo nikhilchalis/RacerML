ee7e7b0f62df7e285130d994301601a582cca408
nikhilchalis <nikhilchalis@gmail.com>
2023-01-21 14:47:51 +1100

    adding basic classes for sprites and racer

--- classes.py
+++ classes.py
@@ -8,4 +8,11 @@ class Sprite:
 
 
 class Racer(Sprite):
-    pass
+    def __init__(self, type, color, pos, vel, acc, dt):
+        super.__init__(type, color, pos)
+        self.vel = vel
+        self.acc = acc
+        self.dt = dt
+
+    def move(self):
+        self.pos += self.vec*self.dt + 0.5*self.acc*self.dt*self.dt
