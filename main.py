bc186012c28e93fe6c6c61cff34a3f4216f46dfb
nikhilchalis <nikhilchalis@gmail.com>
2025-07-29 12:53:38 +1000

    wip for racer initialisation

--- main.py
+++ main.py
@@ -1,55 +1,96 @@
-import pygame as pg
-import sys
+import game_env
+import pygame
 import numpy as np
-from racer_class import Racer
+from ddqn_keras import DDQNAgent
+from collections import deque
+import random, math
 
-# constants
-SC_WIDTH = 1000
-SC_HEIGHT = 1000
-DT = 0.1
-FPS = 25
-BLACK = (0, 0, 0)
+TOTAL_GAMETIME = 1000 # Max game time for one episode
+N_EPISODES = 10000
+REPLACE_TARGET = 50 
 
+game = game_env.RacingEnv()
+game.fps = 60
 
-def main():
-    pg.init()
-    sc = pg.display.set_mode((SC_WIDTH, SC_HEIGHT))
-    clock = pg.time.Clock()
-    sc.fill(BLACK)
+GameTime = 0 
+GameHistory = []
+renderFlag = False
 
-    p_x = np.random.rand() * SC_WIDTH
-    p_y = np.random.rand() * SC_HEIGHT
+ddqn_agent = DDQNAgent(alpha=0.0005, gamma=0.99, n_actions=5, epsilon=1.00, epsilon_end=0.10, epsilon_dec=0.9995, replace_target= REPLACE_TARGET, batch_size=512, input_dims=19)
 
-    basic_racer = Racer(p_x, p_y, SC_WIDTH, SC_HEIGHT, DT)
-    
-    running = True
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
+# if you want to load the existing model uncomment this line.
+# careful an existing model might be overwritten
+#ddqn_agent.load_model()
+
+ddqn_scores = []
+eps_history = []
+
+def run():
+
+    for e in range(N_EPISODES):
         
-        pressed = pg.key.get_pressed()
-        if (pressed[pg.K_UP] or pressed[pg.K_w]): 
-            basic_racer.speed_up()
-        elif (pressed[pg.K_DOWN] or pressed[pg.K_s]): 
-            basic_racer.slow_down()
-        elif (pressed[pg.K_LEFT] or pressed[pg.K_a]): 
-            basic_racer.turn_left()
-        elif (pressed[pg.K_RIGHT] or pressed[pg.K_d]): 
-            basic_racer.turn_right()
-        elif (pressed[pg.K_SPACE]): 
-            basic_racer.warp = True
+        game.reset() #reset env 
+
+        done = False
+        score = 0
+        counter = 0
         
+        observation_, reward, done = game.step(0)
+        observation = np.array(observation_)
 
-        clock.tick(FPS)
-        pg.display.flip()
-        sc.fill(BLACK)
-
-        basic_racer.move()
-        basic_racer.wrap()
-        sc.blit(basic_racer.image, basic_racer.pos)
+        gtime = 0 # set game time back to 0
         
-    
+        renderFlag = False # if you want to render every episode set to true
 
-if __name__ == "__main__":
-    main()
+        if e % 10 == 0 and e > 0: # render every 10 episodes
+            renderFlag = True
+
+        while not done:
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: 
+                    return
+
+            action = ddqn_agent.choose_action(observation)
+            observation_, reward, done = game.step(action)
+            observation_ = np.array(observation_)
+
+            # This is a countdown if no reward is collected the car will be done within 100 ticks
+            if reward == 0:
+                counter += 1
+                if counter > 100:
+                    done = True
+            else:
+                counter = 0
+
+            score += reward
+
+            ddqn_agent.remember(observation, action, reward, observation_, int(done))
+            observation = observation_
+            ddqn_agent.learn()
+            
+            gtime += 1
+
+            if gtime >= TOTAL_GAMETIME:
+                done = True
+
+            if renderFlag:
+                game.render(action)
+
+        eps_history.append(ddqn_agent.epsilon)
+        ddqn_scores.append(score)
+        avg_score = np.mean(ddqn_scores[max(0, e-100):(e+1)])
+
+        if e % REPLACE_TARGET == 0 and e > REPLACE_TARGET:
+            ddqn_agent.update_network_parameters()
+
+        if e % 10 == 0 and e > 10:
+            ddqn_agent.save_model()
+            print("save model")
+            
+        print('episode: ', e,'score: %.2f' % score,
+              ' average score %.2f' % avg_score,
+              ' epsolon: ', ddqn_agent.epsilon,
+              ' memory size', ddqn_agent.memory.mem_cntr % ddqn_agent.memory.mem_size)   
+
+run()        
